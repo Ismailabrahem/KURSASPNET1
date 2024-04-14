ec572e22c6286e595a5096f1bc4e170050332f7b
Ismailabrahem <143405336+Ismailabrahem@users.noreply.github.com>
2024-04-14 23:22:10 +0200

    first commit

---
+++ Silicon_1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Silicon_1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly DataContext _dataContext;
+    private readonly HttpClient _httpClient;
+
+    public HomeController(DataContext dataContext, IHttpClientFactory httpClientFactory)
+    {
+        _dataContext = dataContext;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+}
